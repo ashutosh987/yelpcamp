fca403c8bf660ba7c5c72c435c27c7831bcf55b0
ashutosh987 <guptaashutosh427@gmail.com>
2019-11-09 09:35:03 +0530

    Error corrected

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
-var passportLocalMongoose=require("passport-Local-mongoose");
-var UserSchema=new mongoose.Schema({
-username:String,
-password:String
+var passportLocalMongoose = require("passport-local-mongoose");
+var UserSchema = new mongoose.Schema({
+  username: String,
+  password: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User",UserSchema);
+module.exports = mongoose.model("User", UserSchema);
